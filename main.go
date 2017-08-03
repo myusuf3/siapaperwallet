fd1c762ec16fa8a2ba824460ab0ab56b1abf7f93
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2017-08-03 16:46:36 -0400

    load the page get new seed

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net"
 	"net/http"
-	"os"
 
 	"github.com/NebulousLabs/Sia/crypto"
 	"github.com/NebulousLabs/Sia/modules"
@@ -15,6 +13,11 @@ import (
 )
 
 const nAddresses = 20
+
+type Secret struct {
+	Seed      string
+	Addresses []types.UnlockHash
+}
 
 // getAddress returns an address generated from a seed at the index specified
 // by `index`.
@@ -26,53 +29,44 @@ func getAddress(seed modules.Seed, index uint64) types.UnlockHash {
 	}.UnlockHash()
 }
 
-func main() {
-	// generate a seed and a few addresses from that seed
+func GenerateNewSeedAddress() (*Secret, error) {
 	var seed modules.Seed
 	fastrand.Read(seed[:])
 	var addresses []types.UnlockHash
 	seedStr, err := modules.SeedToString(seed, "english")
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return nil, err
 	}
 	for i := uint64(0); i < nAddresses; i++ {
 		addresses = append(addresses, getAddress(seed, i))
 	}
 
-	templateData := struct {
-		Seed      string
-		Addresses []types.UnlockHash
-	}{
+	templateData := &Secret{
 		Seed:      seedStr,
 		Addresses: addresses,
 	}
-	t, err := template.New("output").ParseFiles("./templates/secret.html")
-	if err != nil {
-		log.Fatal(err)
-	}
+	return templateData, nil
+}
+
+func main() {
+	// generate a seed and a few addresses from that seed
+
+	t := template.Must(template.ParseFiles("templates/secret.html"))
 	l, err := net.Listen("tcp", "localhost:8087")
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 
 	done := make(chan struct{})
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		templateData, err := GenerateNewSeedAddress()
+		if err != nil {
+			log.Fatal(err)
+		}
 		t.Execute(w, templateData)
 		l.Close()
-		close(done)
 	})
 	go http.Serve(l, handler)
-
-	// err = open.Run("http://localhost:8087")
-	if err != nil {
-		// fallback to console, clean up the server and exit
-		l.Close()
-		fmt.Println("Seed:", seedStr)
-		fmt.Println("Addresses:")
-		for _, address := range addresses {
-			fmt.Println(address)
-		}
-		os.Exit(0)
-	}
 	<-done
 }
